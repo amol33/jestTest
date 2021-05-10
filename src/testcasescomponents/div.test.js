df4473a616dae4c2d48fbd3d885711f22477d299
Amol Jugdar <amoljugdar03@gmail.com>
2021-05-10 04:43:21 -0400

    addd more files

---
+++ src/testcasescomponents/div.test.js
@@ -0,0 +1,8 @@
+import div from './div'
+
+it("When a & b is number",()=>
+{
+    const result=div(20,10);
+
+   expect(result).toBe(2);
+});
---
+++ src/testcasescomponents/mult.test.js
@@ -0,0 +1,8 @@
+import mult from './mult'
+
+it("When a & b is number",()=>
+{
+    const result=mult(20,10);
+
+   expect(result).toBe(200);
+});
